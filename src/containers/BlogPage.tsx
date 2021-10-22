80854f7d36cfa1148ceab86ef996a608c5fbdd0b
Sey-Uki <abakarova.diana99@gmail.com>
2021-10-22 10:49:28 +0300

    my second step

--- src/containers/BlogPage.tsx
+++ src/containers/BlogPage.tsx
@@ -2,6 +2,15 @@ import { Button } from "antd";
 import React from "react";
 import { posts } from "../shared/projectData";
 import { AddPostForm } from "./components/AddPostForm";
+import "./BlogPage.css";
+import {
+  HeartOutlined,
+  DeleteOutlined,
+  ExclamationCircleOutlined,
+} from "@ant-design/icons";
+import { Modal } from "antd";
+
+const { confirm } = Modal;
 
 export interface FormInputs {
   title: string,
@@ -11,7 +20,8 @@ export interface FormInputs {
 interface Post {
   id: number,
   title: string,
-  description: string
+  description: string,
+  liked: boolean
 }
 
 interface MyState {
@@ -22,63 +32,106 @@ interface MyState {
 export class BlogPage extends React.Component {
   state: MyState = {
     showAddForm: false,
-    posts
-  }
-   
-  handleAddFormShow = () => {
+    posts,
+  };
+
+  handleAddFormShow() {
     this.setState({
       showAddForm: true,
     });
-  };
+  }
 
-  handleAddFormHide = () => {
+  handleAddFormHide() {
     this.setState({
       showAddForm: false,
     });
-  };
+  }
 
-  addPost = (values: FormInputs) => {
+  addPost(values: FormInputs) {
     const temp = [...this.state.posts];
     const newPost: Post = {
       id: posts.length + 1,
       title: values.title,
       description: values.description,
+      liked: false,
     };
-    temp.push(newPost)
+    temp.push(newPost);
 
     this.setState((state: MyState) => {
-      return {posts: [...state.posts, newPost]};
+      return { posts: [...state.posts, newPost] };
     });
 
-    this.handleAddFormHide()
+    this.handleAddFormHide();
+  }
+
+  likePost(pos: number) {
+    const temp = [...this.state.posts];
+    temp[pos].liked = !temp[pos].liked;
+
+    this.setState((state: MyState) => {
+      return { posts: temp };
+    });
+  }
+
+  deletePost(pos: number) {
+    const temp = [...this.state.posts];
+
+    const onOk = () => {
+      console.log("OK");
+      temp.splice(pos, 1);
+      this.setState((state: MyState) => {
+        return { posts: temp };
+      });
+    };
+
+    confirm({
+      title: "Вы хотите удалить этот пост?",
+      icon: <ExclamationCircleOutlined />,
+      content: "Действия необратимы",
+      okText: "Да",
+      okType: "danger",
+      cancelText: "Нет",
+      onOk,
+      onCancel() {
+        console.log("Cancel");
+      },
+    });
   }
 
   render() {
-    const blogPosts = this.state.posts.map((item) => {
+    const blogPosts = this.state.posts.map((item, pos) => {
       return (
         <div key={item.id} className="post">
           <h2>{item.title}</h2>
           <p>{item.description}</p>
+          <button className="likeBtn" onClick={() => this.likePost(pos)}>
+            <HeartOutlined
+              style={{ color: item.liked ? "crimson" : "black" }}
+            />
+          </button>
+          <button className="likeBtn" onClick={() => this.deletePost(pos)}>
+            <DeleteOutlined />
+          </button>
         </div>
       );
     });
     return (
       <div className="blogPage">
         {this.state.showAddForm && (
-          <AddPostForm    
+          <AddPostForm
             handleAddFormHide={this.handleAddFormHide}
             showAddForm={this.state.showAddForm}
             addPost={this.addPost}
           />
         )}
-         <h1>Simple Blog</h1>
-         <div className="addNewPost">
-            <Button type="primary" onClick={this.handleAddFormShow}>
-              Создать новый пост
-            </Button>
-          </div>
-         <div className="posts">{blogPosts}</div>
+        <h1>Simple Blog</h1>
+        <div className="addNewPost">
+          <Button type="primary" onClick={this.handleAddFormShow}>
+            Создать новый пост
+          </Button>
+        </div>
+        <div className="posts">{blogPosts}</div>
       </div>
-    )
+    );
   }
 }
